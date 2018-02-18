1c30d60aa164e7dde4555b3d5a83a25181096f69
Destiny Brown <dbbrown14@students.desu.edu>
2018-02-17 21:24:46 -0500

    In Class Exercises

---
+++ In Class Exercises/Assets/Scripts/S_Cannonball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Cannonball : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject go = other.gameObject;
+        if (go.tag == "Player")
+        {
+            Debug.Log("The player was hit!");
+        }
+    }
+    
+}
---
+++ In Class Exercises/Assets/Scripts/S_Turret.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Turret : MonoBehaviour {
+
+    public GameObject rotatable;
+    public GameObject spawnPt;
+    public GameObject cannonball;
+
+    private void Start()
+    {
+       StartCoroutine(shootCannonball());
+    }
+
+    void Update()
+    {
+        float a = 70 * Time.time;
+        rotatable.transform.localRotation = Quaternion.Euler(0, a, 0);
+    }
+
+    private IEnumerator shootCannonball()
+    {
+        while (true)
+        {
+            GameObject go = Instantiate(cannonball);
+            go.transform.localPosition = spawnPt.transform.position;
+            go.transform.localRotation = spawnPt.transform.rotation;
+            go.GetComponent<Rigidbody>().velocity = 2 * spawnPt.transform.forward;
+            Destroy(go, 5f);
+
+            yield return new WaitForSeconds(3F);
+        }
+    }
+
+    
+}
